3e624ff29730a7945ab5d3ad49da6e41fd3354af
Mr. Bharat Roy <bharatroy1005@gmail.comgidjfclsgir>
2024-12-06 12:31:53 +0600

    MoneyContext added

--- src/components/Aunty/Aunty.jsx
+++ src/components/Aunty/Aunty.jsx
@@ -1,12 +1,18 @@
+import { useContext } from "react";
 import Cousin from "../Cousin/Cousin";
+import { MoneyContext } from "../Grandpa/Grandpa";
 
 const Aunty = ({asset}) => {
+    const [money, setMoney] = useContext(MoneyContext);
     return (
         <div> 
             <h2>Aunty</h2>
-            <section className="flex">
+            <section className="flex"> 
             <Cousin name={'Mafuga'} asset={asset}></Cousin> 
             <Cousin name={'kanta bai'}></Cousin> 
+            </section>
+            <section>
+                <p>money:{money}</p>
             </section>
         </div>
     );
--- src/components/Grandpa/Grandpa.jsx
+++ src/components/Grandpa/Grandpa.jsx
@@ -1,17 +1,21 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import Aunty from "../Aunty/Aunty";
 import Dad from "../Dad/Dad";
 import Uncle from "../Uncle/Uncle";
 import './Grandpa.css';
 
 export const AssetContext = createContext('gold');
+export const MoneyContext = createContext(1000);
 
 const Grandpa = () => {
     const asset = 'Dimond';
+    const [money, setMoney] = useState(1000);
     return (
         <div className="grandpa">
             <div>
                 <h1>Grandpa</h1>
+                <h2>Net money:{money}</h2>
+                <MoneyContext.Provider value={[money, setMoney]}>
                 <AssetContext.Provider value="gold">
                     <section className="flex">
                         <Dad asset={asset}></Dad>
@@ -19,6 +23,7 @@ const Grandpa = () => {
                         <Uncle></Uncle>
                     </section>
                 </AssetContext.Provider>
+                </MoneyContext.Provider>
             </div>
         </div>
     );
